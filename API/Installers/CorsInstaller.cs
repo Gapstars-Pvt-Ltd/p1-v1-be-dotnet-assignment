21354294d7e0138bf483226ca1cd9bcd906fd1bd
HP <HP@LAPTOP-R5L5IOK2>
2023-03-19 10:38:05 +0530

    Add Cors Support to the Api

--- API/Installers/CorsInstaller.cs
+++ API/Installers/CorsInstaller.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Infrastructure.Cors;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System;
 
 namespace API.Installers
 {
     public class CorsInstaller : IInstaller
     {
+        private const string CorsPolicy = nameof(CorsPolicy);
         public void InstallService(IServiceCollection services, IConfiguration configuration)
         {
-            throw new System.NotImplementedException();
+            //get cors setting from configuartion 
+            var corsSettings = configuration.GetSection(nameof(CorsSettings)).Get<CorsSettings>();
+            var origins = new List<string>();
+            if (corsSettings.Swagger is not null)
+                origins.AddRange(corsSettings.Swagger.Split(';', StringSplitOptions.RemoveEmptyEntries));
+         
+            // register cosrs policy
+            services.AddCors(opt =>
+                opt.AddPolicy(CorsPolicy, policy =>
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .WithOrigins(origins.ToArray())));
         }
     }
 }
